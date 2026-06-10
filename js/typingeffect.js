8057f5137870a5979fcc1537544fe8365da27d75
IamTirtha <tsd2580@gmail.com>
2026-06-10 21:46:31 +0600

    Banner section created

---
+++ js/typingeffect.js
@@ -0,0 +1,7 @@
+var typed = new Typed('#element', {
+    strings: ['Data analyst', 'Development', 'Tech Enthusiast','S/W Tester'],
+    typeSpeed: 100, // in millisec
+    // @property {boolean}
+    loop: true,          // for looping
+    loopCount: Infinity, // loop how many times
+});
